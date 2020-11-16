d727d737dd323bf6c2d3f8c98899eb86c8508538
Fred Melo <fred.melo@kobold.com.br>
2020-11-16 00:51:05 -0300

    adiciona arquivo de rascunho

---
+++ rascunho.c
@@ -0,0 +1,37 @@
+
+int recuperarLinha(int linha) {
+	static const char filename[] = "file.txt";
+	FILE *file = fopen(filename, "r");
+	int count = 0;
+	if ( file != NULL )
+	{
+		char line[256]; /* or other suitable maximum line size */
+		while (fgets(line, sizeof line, file) != NULL) /* read a line */
+		{
+			if (count == lineNumber)
+			{
+				//use line or in a function return it
+				//in case of a return first close the file with "fclose(file);"
+			}
+			else
+			{
+				count++;
+			}
+		}
+		fclose(file);
+	}
+	else
+	{
+		//file doesn't exist
+	}
+}
+
+// Função para cadastrar paciente 
+int cadastraPaciente() {
+
+}
+
+// Função para gerar relatório
+int geraRelatorio() {
+
+}
